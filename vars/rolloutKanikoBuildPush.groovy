b8e396682fe641824d59d17539b9f8d2004022a3
Naveen Modala <mnaveen0725@gmail.com>
2022-07-26 09:31:32 -0500

    Initialize GA-Rollout

---
+++ vars/rolloutKanikoBuildPush.groovy
@@ -0,0 +1,33 @@
+// vars/kanikoBuildPush.groovy
+def call(String imageName, String imageTag = env.BUILD_NUMBER, String gcpProject = "core-workshop", String target = ".", String dockerFile="Dockerfile", Closure body) {
+  def dockerReg = "gcr.io/${gcpProject}"
+  //imageName = "helloworld-nodejs"
+  def label = "kaniko-${UUID.randomUUID().toString()}"
+  def podYaml = libraryResource 'podtemplates/dockerBuildPush.yml'
+  def folderName = ""
+  podTemplate(name: 'kaniko', label: label, yaml: podYaml, inheritFrom: 'default-jnlp', nodeSelector: 'type=agent') {
+    node(label) {
+      if(imageName=="rollout-js"){
+        folderName = "frontend-spring-boot-react-crud-full-stack-with-maven"
+      }
+      else if(imageName=="rollout-java"){
+        folderName = "backend-spring-boot-react-crud-full-stack-with-maven"
+      }
+      body()
+      rolloutImageNameTag()
+      gitShortCommit()
+      def repoName = env.IMAGE_REPO.toLowerCase()
+      container(name: 'kaniko', shell: '/busybox/sh') {
+        withEnv(['PATH+EXTRA=/busybox:/kaniko']) {
+          // echo """#!/busybox/sh
+          //   /kaniko/executor -f ${pwd()}/${folderName}/${dockerFile} -c ${pwd()}/${folderName} --build-arg context=${repoName} --build-arg buildNumber=${BUILD_NUMBER} --build-arg shortCommit=${env.SHORT_COMMIT} --build-arg commitAuthor=${env.COMMIT_AUTHOR} -d ${dockerReg}/${imageName}:${repoName}-${BUILD_NUMBER}
+          // """
+          sh """#!/busybox/sh
+            /kaniko/executor -f ${pwd()}/${folderName}/${dockerFile} -c ${pwd()}/${folderName} --build-arg context=${repoName} --build-arg buildNumber=${BUILD_NUMBER} --build-arg shortCommit=${env.SHORT_COMMIT} --build-arg commitAuthor=${env.COMMIT_AUTHOR} -d ${dockerReg}/${imageName}:${repoName}-${BUILD_NUMBER}
+          """
+        }
+      }
+      publishEvent event:jsonEvent("{'eventType':'containerImagePush', 'image':'${dockerReg}/${imageName}:${repoName}-${BUILD_NUMBER}'}"), verbose: true
+    }
+  }
+}
---
+++ vars/rolloutKubeDeploy.groovy
@@ -0,0 +1,57 @@
+// vars/kubeDeploy.groovy
+def call(imageName, imageTag, githubCredentialId, repoOwner) {
+    def label = "kubectl"
+    def podYaml = libraryResource 'podtemplates/kubeDeploy.yml'
+    def deployYaml = libraryResource "k8s/${imageName}BasicDeploy.yml"
+    //def repoName = env.IMAGE_REPO.toLowerCase()
+    def envStagingRepo = "environment_staging"
+    def pullMaster = true
+    
+    podTemplate(name: 'kubectl', label: label, yaml: podYaml) {
+      node(label) {
+        //create environment repo for prod if it doesn't already exist
+        echo githubCredentialId
+        withCredentials([usernamePassword(credentialsId: githubCredentialId, usernameVariable: 'USERNAME', passwordVariable: 'ACCESS_TOKEN')]) {
+          echo repoOwner
+          echo envStagingRepo
+          def repoNotExists = sh(script: '''
+              curl -H "Authorization: token $ACCESS_TOKEN" https://api.github.com/repos/$repoOwner/environment_staging | jq 'contains({message: "Not Found"})'
+            ''', returnStdout: true)
+          echo "Repo exists? ${repoNotExists}"
+          if(repoNotExists) {
+            sh(script: """
+                curl -H "Authorization: token $ACCESS_TOKEN" --data '{"name":"${envStagingRepo}"}' https://api.github.com/orgs/${repoOwner}/repos
+              """)
+             pullMaster = false 
+          }
+          echo "Pull master? ${pullMaster}"
+          //curl -H "Authorization: token ACCESS_TOKEN" --data '{"name":""}' https://api.github.com/orgs/ORGANISATION_NAME/repos
+        }
+        writeFile file: "deploy.yml", text: deployYaml
+        sh("sed -i.bak 's#REPLACE_IMAGE_TAG#gcr.io/core-workshop/${imageName}:${repoName}-${BUILD_NUMBER}#' deploy.yml")
+        sh("sed -i.bak 's#REPLACE_SERVICE_NAME#${repoName}#' deploy.yml")
+        withCredentials([usernamePassword(credentialsId: githubCredentialId, usernameVariable: 'USERNAME', passwordVariable: 'ACCESS_TOKEN')]) {
+          sh """
+            git init
+            git config user.email "deployBot@cb-sa.io"
+            git config user.name "${USERNAME}"
+            git remote add origin https://${USERNAME}:${ACCESS_TOKEN}@github.com/${repoOwner}/${envStagingRepo}.git
+          """
+          if(pullMaster) {
+            sh 'git pull origin master'
+          } else {
+            sh 'git add deploy.yml'
+          }
+          sh """
+            git commit -a -m 'updating ${envStagingRepo} deployment for ${repoName}'
+            git push -u origin master
+          """
+        }
+        container("kubectl") {
+          sh "cat deploy.yml"
+          sh "kubectl apply -f deploy.yml"
+          sh "echo 'deployed to http://staging.cb-sa.io/${repoName}/'"
+        }
+      }
+    }
+}
